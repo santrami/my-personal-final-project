7c4c800f34f163dc708554a599ed791883614be1
santrami <santrami@hotmail.com>
2023-07-30 10:05:28 +0200

    fixing space between logo and items on mobile sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -88,7 +88,7 @@ const Sidebar = ({ apiLimitCount = 0, isPro = false }: SidebarProps) => {
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-0 flex-1">
-        <Link href="/dashboard" className="flex items-center pl-3 mb-14">
+        <Link href="/dashboard" className="flex items-center pl-3 mb-5">
           <div className="relative w-16 h-16 mr-4">
             <Image fill alt="logo" src="/logo.png" />
           </div>
